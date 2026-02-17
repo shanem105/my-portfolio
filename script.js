698f851248f1b30af39a16a8f3865897bada5f4f
Shane <shanemack02@gmail.com>
2026-02-17 13:59:13 -0500

    finalizing stuff

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 /* script.js */
 
-// Core Skills
-const text = "Network Defense | Infrastructure Hardening | Traffic Analysis";
+// The typing effect text
+const text = "IT Operations | Endpoint Security | Network Defense";
 
-// speed in ms
 const speed = 50; 
-
 let i = 0;
 
 function typeWriter() {
-    // Look for the element with id="typewriter" in HTML
     const element = document.getElementById("typewriter");
     
-    // Only run if the element exists and we haven't finished typing
     if (element && i < text.length) {
         element.innerHTML += text.charAt(i);
         i++;
@@ -20,5 +16,4 @@ function typeWriter() {
     }
 }
 
-// Start the function when the window loads
 window.onload = typeWriter;
