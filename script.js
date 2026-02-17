f73442e9438b577f9a95c9d240da81d35deae6db
Shane <shanemack02@gmail.com>
2026-02-17 14:18:39 -0500

    Final (hopefully)

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 /* script.js */
-
-// The typing effect text
 const text = "IT Operations | Endpoint Security | Network Defense";
-
 const speed = 50; 
 let i = 0;
 
 function typeWriter() {
     const element = document.getElementById("typewriter");
-    
     if (element && i < text.length) {
         element.innerHTML += text.charAt(i);
         i++;
